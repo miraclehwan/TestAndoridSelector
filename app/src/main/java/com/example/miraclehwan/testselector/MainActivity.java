1d0549c5ac02753c496ac9b38e0be17c182119b2
Daehwan Kim <daehwan7298@gmail.com>
2018-05-22 22:36:57 +0900

    changed AndroidStudio 3.1.2 -> 3.0.1 for using DataBinding

--- app/src/main/java/com/example/miraclehwan/testselector/MainActivity.java
+++ app/src/main/java/com/example/miraclehwan/testselector/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.miraclehwan.testselector;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,36 +9,24 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.miraclehwan.testselector.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    int currentPosition = R.id.menu1_layout;
+    ActivityMainBinding binding;
 
-    LinearLayout menu1, menu2, menu3;
-    TextView menu1_text, menu2_text, menu3_text;
-    ImageView menu1_image, menu2_image, menu3_image;
+    int currentPosition = R.id.menu1_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        menu1 = (LinearLayout) findViewById(R.id.menu1_layout);
-        menu2 = (LinearLayout) findViewById(R.id.menu2_layout);
-        menu3 = (LinearLayout) findViewById(R.id.menu3_layout);
+        binding.menu1Layout.setOnClickListener(clickListener);
+        binding.menu2Layout.setOnClickListener(clickListener);
+        binding.menu3Layout.setOnClickListener(clickListener);
 
-        menu1_text = (TextView) findViewById(R.id.menu1_textview);
-        menu2_text = (TextView) findViewById(R.id.menu2_textview);
-        menu3_text = (TextView) findViewById(R.id.menu3_textview);
-
-        menu1_image = (ImageView) findViewById(R.id.menu1_imageview);
-        menu2_image = (ImageView) findViewById(R.id.menu2_imageview);
-        menu3_image = (ImageView) findViewById(R.id.menu3_imageview);
-
-        menu1.setOnClickListener(clickListener);
-        menu2.setOnClickListener(clickListener);
-        menu3.setOnClickListener(clickListener);
-
-        SetSelect(menu1_text, menu1_image);
+        SetSelect(binding.menu1Textview, binding.menu1Imageview);
 
     }
 
@@ -46,19 +35,19 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             if (currentPosition != v.getId()){
                 if (currentPosition == R.id.menu1_layout){
-                    SetunSelect(menu1_text, menu1_image);
+                    SetunSelect(binding.menu1Textview, binding.menu1Imageview);
                 }else if (currentPosition == R.id.menu2_layout){
-                    SetunSelect(menu2_text, menu2_image);
+                    SetunSelect(binding.menu2Textview, binding.menu2Imageview);
                 }else if (currentPosition == R.id.menu3_layout){
-                    SetunSelect(menu3_text, menu3_image);
+                    SetunSelect(binding.menu3Textview, binding.menu3Imageview);
                 }
 
                 if (v.getId() == R.id.menu1_layout){
-                    SetSelect(menu1_text, menu1_image);
+                    SetSelect(binding.menu1Textview, binding.menu1Imageview);
                 }else if (v.getId() == R.id.menu2_layout){
-                    SetSelect(menu2_text, menu2_image);
+                    SetSelect(binding.menu2Textview, binding.menu2Imageview);
                 }else if (v.getId() == R.id.menu3_layout){
-                    SetSelect(menu3_text, menu3_image);
+                    SetSelect(binding.menu3Textview, binding.menu3Imageview);
                 }
                 currentPosition = v.getId();
             }else{
